e7f7475973390879da9ee10e31ef9ff91524d600
Rahil Rashid <rrashid@sparkcognition.com>
2024-11-15 12:35:55 +0530

    Added Daisy UI Lib and imported Navbar from the library

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
     <div>
-      <h1 className="text-blue-400 font-bold">Hello World</h1>
+      <Navbar />
     </div>
   );
 };
