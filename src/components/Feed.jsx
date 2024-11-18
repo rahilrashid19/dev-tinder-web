06573d4d341fc80e3be2e9f69f8644df5d3723be
Rahil Rashid <rrashid@sparkcognition.com>
2024-11-18 16:49:59 +0530

    added new card type and footer

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -23,25 +23,16 @@ const Feed = () => {
     <div className="flex">
       {feedData?.map((feed) => {
         return (
-          <div className="max-w-lg mx-auto mt-10" key={feed._id}>
-            <div className="card card-side bg-white shadow-xl border border-gray-200 rounded-lg overflow-hidden">
-              {/* Profile Picture */}
-              <figure className="w-1/3">
-                <img
-                  src={feed.profilePicture || "/default-avatar.png"}
-                  alt={`${feed.firstName} ${feed.lastName}`}
-                  className="object-cover w-full h-full"
-                />
-              </figure>
-
-              {/* Profile Details */}
-              <div className="card-body w-2/3 p-6 flex flex-col justify-center">
-                <h2 className="card-title text-2xl font-bold text-gray-800">
-                  {feed.firstName + " " + feed.lastName}
-                </h2>
-                <p className="text-gray-600 mt-2">
-                  {feed.bio || "This user has not added a bio yet."}
-                </p>
+          <div className="card bg-base-100 w-96 shadow-xl" key={feed._id}>
+            <figure>
+              <img src={feed.profilePicture} alt="user" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{feed.firstName + feed.lasName}</h2>
+              <p>{feed.bio}</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Interested</button>
+                <button className="btn btn-primary">Ignore</button>
               </div>
             </div>
           </div>
